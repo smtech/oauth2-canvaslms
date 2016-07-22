b27f48b9f143b567fb86aa0adbbc3648247c68f5
Seth Battis <sethbattis@stmarksschool.org>
2016-07-22 08:53:25 -0600

    PSR-2
    Address [this issue](https://github.com/smtech/oauth2-canvaslms/issues/2).

--- src/Provider/CanvasLMS.php
+++ src/Provider/CanvasLMS.php
@@ -10,46 +10,72 @@ use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
 use Psr\Http\Message\ResponseInterface;
 use smtech\OAuth2\Client\Provider\CanvasLMSResourceOwner;
 
-class CanvasLMS extends AbstractProvider {
+class CanvasLMS extends AbstractProvider
+{
 
     use ArrayAccessorTrait,
         BearerAuthorizationTrait;
 
-    protected $canvasInstanceUrl, $purpose, $grantType;
+    /**
+     * URL of Canvas Instance (e.g. https://canvas.instructure.com)
+     * @var string
+     */
+    protected $canvasInstanceUrl;
 
-    protected function getAuthorizationParameters(array $options) {
+    /**
+     * Human-readable purpose for which API access token will be issued
+     * @var string
+     */
+    protected $purpose;
+
+    /**
+     * Type of token requested (`authorization_code` or `refresh_token`)
+     * @var [type]
+     */
+    protected $grantType;
+    /* TODO https://github.com/smtech/oauth2-canvaslms/issues/1 */
+
+    protected function getAuthorizationParameters(array $options)
+    {
         $options = parent::getAuthorizationParameters($options);
         $options['purpose'] = $this->purpose;
         return $options;
     }
 
-    public function getBaseAuthorizationUrl() {
+    public function getBaseAuthorizationUrl()
+    {
         return "{$this->canvasInstanceUrl}/login/oauth2/auth";
     }
 
-    public function getBaseAccessTokenUrl(array $params) {
+    public function getBaseAccessTokenUrl(array $params)
+    {
         return "{$this->canvasInstanceUrl}/login/oauth2/token";
     }
 
-    public function getResourceOwnerDetailsUrl(AccessToken $token) {
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
         return "{$this->canvasInstanceUrl}/api/v1/users/self";
     }
 
-    public function getDefaultScopes() {
+    public function getDefaultScopes()
+    {
         return [];
     }
 
-    public function checkResponse(ResponseInterface $response, $data) {
+    public function checkResponse(ResponseInterface $response, $data)
+    {
         if (!empty($data['error'])) {
             throw new IdentityProviderException($data['error_description'], $response->getStatusCode(), $response);
         }
     }
 
-    public function createResourceOwner(array $response, AccessToken $token) {
+    public function createResourceOwner(array $response, AccessToken $token)
+    {
         return new CanvasLMSResourceOwner($response);
     }
 
-    public function getAccessTokenRequest(array $params) {
+    public function getAccessTokenRequest(array $params)
+    {
         $request = parent::getAccessTokenRequest($params);
         $uri = $request->getUri()->withUserInfo($this->clientId, $this->clientSecret);
         return $request->withUri($uri);
--- src/Provider/CanvasLMSResourceOwner.php
+++ src/Provider/CanvasLMSResourceOwner.php
@@ -5,59 +5,73 @@ namespace smtech\OAuth2\Client\Provider;
 use League\OAuth2\Client\Provider\ResourceOwnerInterface;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 
-class CanvasLMSResourceOwner implements ResourceOwnerInterface {
+class CanvasLMSResourceOwner implements ResourceOwnerInterface
+{
 
     protected $response;
 
-    public function __construct(array $response = []) {
+    public function __construct(array $response = [])
+    {
         $this->response = $response;
     }
 
-    public function getId() {
+    public function getId()
+    {
         return $this->response['id'];
     }
 
-    public function getName() {
+    public function getName()
+    {
         return $this->response['name'];
     }
 
-    public function getSortableName() {
+    public function getSortableName()
+    {
         return $this->response['sortable_name'];
     }
 
-    public function getShortName() {
+    public function getShortName()
+    {
         return $this->response['short_name'];
     }
 
-    public function getSISUserId() {
+    public function getSISUserId()
+    {
         return $this->response['sis_user_id'];
     }
 
-    public function getIntegrationId() {
+    public function getIntegrationId()
+    {
         return $this->response['integration_id'];
     }
 
-    public function getSISLoginId() {
+    public function getSISLoginId()
+    {
         return $this->response['sis_login_id'];
     }
 
-    public function getLoginId() {
+    public function getLoginId()
+    {
         return $this->response['login_id'];
     }
 
-    public function getAvatarUrl() {
+    public function getAvatarUrl()
+    {
         return $this->response['avatar_url'];
     }
 
-    public function getLocale() {
+    public function getLocale()
+    {
         return $this->response['locale'];
     }
 
-    public function getPermissions() {
+    public function getPermissions()
+    {
         return $this->response['permissions'];
     }
 
-    public function toArray() {
+    public function toArray()
+    {
         return $this->response;
     }
 }
